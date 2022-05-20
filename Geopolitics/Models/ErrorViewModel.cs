1cb0e3770e8f9cfc76a3faa05910bc9834133ecf
Igor <igor050198@gmail.com>
2022-05-20 09:22:40 -0300

    Primeiro commit, projeto criado

---
+++ Geopolitics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Geopolitics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
